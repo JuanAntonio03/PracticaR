d388bb50d985388583b5cdde646027352af29546
JuanAntonio03 <33105963+JuanAntonio03@users.noreply.github.com>
2017-10-26 17:00:24 +0200

    Add files via upload

---
+++ R/dummys.R
@@ -0,0 +1,21 @@
+
+#' @title sumando
+#' @description Esta es una funcion que tiene dos argumentos y los suma .
+#' @param x e y  numeros
+#' @details Esta funcion es dummy y no sirve para nada practicamente.....
+#' @export
+sumando <- function(x,y)  {
+  x+y
+}
+  
+
+#' @title Multiplicar dos números
+#' @description Esta es una funcion de ejemplo para multiplicar.
+#' @param x e y números
+#' @details Esta funcion es dummy y no sirve para nada practicamente.....
+#' @export
+multiplicando <- function(x,y)  {
+  x*y
+}
+
+
